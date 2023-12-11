893cc8e70e168eb98232c677cc97ee4f93785b2e
Andy Pradityo <andy.pradityo@djarum.com>
2023-12-11 09:16:24 +0700

    Added my project

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask! [version 1]"
 
 if __name__ == "__main__":
     # Please do not set debug=True in production
